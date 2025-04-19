6f3dfe809aa5e90bd488d7fec24eba504dd7d037
F16TH <lyz17373572269@gmail.com>
2025-04-20 00:05:48 +0800

    Django页面开发相关练习，本地测试完成，准备推送到vps测试

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -17,8 +17,8 @@ from dotenv import load_dotenv
 load_dotenv()  # 加载.env文件
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+#.parent用于回溯到上级目录
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'yourapp',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
