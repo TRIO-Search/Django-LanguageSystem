21d6c723f01ea0695215392ea4a20b265ba1c202
F16TH <lyz17373572269@gmail.com>
2025-04-22 17:46:46 +0800

    注册用户模型和个人主页小更改

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+#注册自定义用户模型
+admin.site.register(CustomUser, UserAdmin)
 
 # Register your models here.
